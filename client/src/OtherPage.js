d853be1ddc249b6de3d4345c25502197ef921fa4
aibazhang <dtjp@tutanota.com>
2022-03-28 21:00:22 +0900

    add: nginx

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
     <div>
